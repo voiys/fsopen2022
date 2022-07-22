502baa0a0e3834b9ebd0224ff65c73028bac0648
Matej <49211125+voiys@users.noreply.github.com>
2022-07-22 22:39:46 +0200

    Update Countries.js

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -4,46 +4,34 @@ import Country from "./Country";
 import CountryList from "./CountryList";
 
 const Countries = ({ nameFilter }) => {
+  const [countries, setCountries] = useState([]);
+  
   useEffect(() => {
     axios.get("https://restcountries.com/v3.1/all").then((response) => {
       setCountries(response.data);
     });
   }, []);
-
-  const [countries, setCountries] = useState([]);
-
-  const renderCountries = (noOfCountries) => {
-    if (noOfCountries > 10) {
-      return <p>Too many matches, specify another filter</p>;
-    } else if (noOfCountries < 10 && noOfCountries > 1) {
-      return countries
-        .filter((country) =>
-          country.name.common.toLowerCase().includes(nameFilter.toLowerCase())
-        )
-        .map((country) => (
-          <CountryList
-            key={country.name.common}
-            countryName={country.name.common}
-          />
-        ));
-    } else {
-      return countries
-        .filter((country) =>
-          country.name.common.toLowerCase().includes(nameFilter.toLowerCase())
-        )
-        .map((country) => (
-          <Country key={country.name.common} country={country} />
-        ));
-    }
-  };
-  return (
-    <>
-      {renderCountries(
-        countries.filter((country) =>
-          country.name.common.toLowerCase().includes(nameFilter.toLowerCase())
-        ).length
-      )}
-    </>
+  
+  const matches = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(nameFilter.toLowerCase())
   );
+  
+  const noOfCountries = matches.length;
+
+
+  if (noOfCountries > 10) {
+    return <p>Too many matches, specify another filter</p>;
+  } else if (noOfCountries < 10 && noOfCountries > 1) {
+    return matches.map((country) => (
+        <CountryList
+          key={country.name.common}
+          countryName={country.name.common}
+        />
+      ));
+  } else {
+    return matches.map((country) => (
+        <Country key={country.name.common} country={country} />
+      ));
+  }
 };
 export default Countries;
